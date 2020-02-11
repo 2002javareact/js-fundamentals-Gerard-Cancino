6094ae0c719a4a8382bc710b22a0ea51488a4910
gerard-cancino <gerard.cancino@gmail.com>
2020-02-11 12:49:49 -0500

    fib problem solved

--- problems/01-fibonacci/01-fibonacci.js
+++ problems/01-fibonacci/01-fibonacci.js
@@ -2,5 +2,11 @@
 Define function: fib(n) 
 Return the nth number in the fibonacci sequence. */
 function fib(n) {
-
+  let map = [0,1];
+  for(let i=2;i<=n;i++){
+    map[i]=map[i-1]+map[i-2];
+  }
+  return map[n];
 }
+
+console.log(fib(8))
