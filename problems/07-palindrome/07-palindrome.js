c4a4b013b0c48142d3029ade340859ce195825f0
gerard-cancino <gerard.cancino@gmail.com>
2020-02-11 14:53:33 -0500

    palindrome

--- problems/07-palindrome/07-palindrome.js
+++ problems/07-palindrome/07-palindrome.js
@@ -2,5 +2,15 @@
 Define function isPalindrome(someStr)
 Return true if someStr is a palindrome, otherwise return false */
 function isPalindrome(someStr) {
-  
+    // O(n/2)
+    // modifying my code from reverse string
+    if(someStr.length===0) return false;
+    const strArray = someStr.split('');
+    for(let i=0;i<Math.floor(strArray.length/2);i++){
+      let j = strArray.length-i-1;
+      if(strArray[i]!==strArray[j]) return false;
+    }
+    return true;
 }
+
+console.log(isPalindrome(''))
