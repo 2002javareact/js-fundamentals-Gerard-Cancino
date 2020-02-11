6c62278365396963dd42fdb42cb2ba7a03b0a173
gerard-cancino <gerard.cancino@gmail.com>
2020-02-11 14:15:38 -0500

    factorial - Needs exceptions

--- problems/04-factorial/04-factorial.js
+++ problems/04-factorial/04-factorial.js
@@ -2,5 +2,15 @@
 Define function: factorial(someNum)
 Use recursion to compute and return the factorial of someNum.  */
 function factorial(sumNum) {
-  
+  if(sumNum===0){
+    return 0;
+  }
+  // sum, current
+  const findFactorialNum = (sum, current) => {
+    if(current!==0) return findFactorialNum(sum*current,current-1);
+    else return sum;
+  } 
+  return findFactorialNum(1,sumNum);
 }
+
+console.log(factorial(5));
