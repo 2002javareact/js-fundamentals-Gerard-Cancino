69958d4aafd148eb699f8fbbcb4f1136b4874334
gerard-cancino <gerard.cancino@gmail.com>
2020-02-11 14:07:42 -0500

    reverse string

--- problems/03-reverse-string/03-reverse-string.js
+++ problems/03-reverse-string/03-reverse-string.js
@@ -2,5 +2,17 @@
 Define function: reverseStr(someStr)
 Reverse and return the String. */
 function reverseStr(someStr) {
-  
+  // O(n/2)
+  const strArray = someStr.split('');
+  for(let i=0;i<Math.floor(strArray.length/2);i++){
+    let j = strArray.length-i-1;
+    switchChar(i,j,strArray);
+  }
+  return strArray.join('');
 }
+function switchChar(first,second,someStr){
+  const temp = someStr[first];
+  someStr[first] = someStr[second];
+  someStr[second] = temp;
+}
+console.log(reverseStr('testingthefunction'))
