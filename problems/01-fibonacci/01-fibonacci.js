9dfa828e690a8643fb9d5615aa0cfa1850996b39
gerard-cancino <gerard.cancino@gmail.com>
2020-02-12 12:49:56 -0500

    rewrote fib to find performance

--- problems/01-fibonacci/01-fibonacci.js
+++ problems/01-fibonacci/01-fibonacci.js
@@ -1,12 +1,38 @@
 /* 1. Fibonacci
 Define function: fib(n) 
 Return the nth number in the fibonacci sequence. */
-function fib(n) {
+function getPerformance(func,n){
+  console.time(`${func.name}`)
+  func(n);
+  console.timeEnd(`${func.name}`)
+}
+function fib1(n) {
   let map = [0,1];
-  for(let i=2;i<=n;i++){
+  for(let i=2;i<n;i++){
     map[i]=map[i-1]+map[i-2];
   }
-  return map[n];
+  return map[n-1]; 
 }
+function fib2(n){
+  let map = [0,1];
+  function fib(index){
+    if(map[index]!==undefined){
+      return map[index]
+    }
+      return fib(index-1)+fib(index-2);
+  }
+  return fib(n-1);
+}
+function run(n){
+  console.log(`1st Alg Result: ${fib1(n)}`)
+  getPerformance(fib1,n)
+  console.log(`2nd Alg Result: ${fib2(n)}`)
+  getPerformance(fib2,n)
+}
+run(5)
+run(20)
 
-console.log(fib(8))
+/*
+  Fib1 was better for larger calculations
+  Fib2 was better for lower numbers
+*/
--- problems/08-shapes/08-shapes.js
+++ problems/08-shapes/08-shapes.js
@@ -21,5 +21,50 @@ Example for printShape("Diamond", 5, "*");
   * 
 */
 function printShape(shape, height, character) {
-  
-}
+  if(shape==='Square') return createSquare(height,character);
+  else if(shape==='Triangle') return createTriangle(height,character);
+  else if(shape==='Diamond') return createDiamond(height,character);
+  else return 'please input either Square, Triangle, Diamond';
+};
+
+const createSquare = (height,character) => {
+  let firstLine = "";
+  let stringShape = "";
+  for(let i=0;i<height;i++){
+    firstLine+=character;
+  }
+  for(let i=0;i<height;i++){
+    stringShape+=firstLine;
+    if(i!==height-1) stringShape+='/n' 
+  }
+  return stringShape;
+};
+const createTriangle = (height,character) => {
+  let stringShape = '';
+  for(let i=0;i<height;i++){
+    for(let j=0;j<i+1;j++){
+      stringShape+=character;
+      if(j===i) stringShape+='/n';
+    }
+  }
+  return stringShape;
+};
+const createDiamond = (height,character) => {
+  /*
+    while + for -> while + for => O(N/2*N/2)+O(M/2*M/2)
+    for2 => O(N2)
+  */
+  const stringShape = [height][height];
+  const middle = Math.floor(height/2);
+  console.log(stringShape)
+  let i = 0;
+  while(i<middle){
+    for(let j=0;j<i;j++){
+      height[i][middle-j]=character;
+    }
+    i++
+  }
+  console.log(stringShape);
+};
+
+createDiamond(3,'$')
